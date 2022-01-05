088bc96460bc05299f0618a167d1a9f43dab6cca
Jeff66 <jeff66ruan@gmail.com>
2022-01-05 11:23:19 +0800

    tailwind support

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,40 +1,13 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
+import 'tailwindcss/tailwind.css';
 
 const Hello = () => {
   return (
-    <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
+    <div className="text-center">
+      <img src={icon} alt="logo" className="h-48 w-48" />
+      <h1 className="text-2xl font-bold text-red-500">electron!</h1>
     </div>
   );
 };
